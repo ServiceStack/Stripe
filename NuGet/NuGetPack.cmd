fc3ec0a4cf3be658fe6808e62228a3195da1324d
Demis Bellot <demis.bellot@gmail.com>
2014-01-18 23:08:14 +0800

    Add NuGet assets

---
+++ NuGet/NuGetPack.cmd
@@ -0,0 +1,2 @@
+SET NUGET=..\src\.nuget\nuget
+%NUGET% pack ServiceStack.Stripe\servicestack.stripe.nuspec -symbols
---
+++ build/copy.bat
@@ -0,0 +1,5 @@
+
+REM SET BUILD=Debug
+SET BUILD=Release
+
+COPY ..\src\Stripe\bin\Release\Stripe.* "..\NuGet\ServiceStack.Stripe\lib\portable-net45+win8+monotouch+monoandroid"

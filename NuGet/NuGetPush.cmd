c3024ac724a99cd49aa945ab417a26eab85a9553
Demis Bellot <demis.bellot@gmail.com>
2014-01-20 10:07:46 +0800

    Update new NuGet config

--- NuGet/NuGetPush.cmd
+++ NuGet/NuGetPush.cmd
@@ -1,3 +1,3 @@
 SET NUGET=..\src\.nuget\nuget
-%NUGET% push ServiceStack.Text.3.9.60.nupkg
-%NUGET% push ServiceStack.Text.3.9.60.symbols.nupkg
+%NUGET% push ServiceStack.Stripe.4.0.8.nupkg
+%NUGET% push ServiceStack.Stripe.4.0.8.symbols.nupkg

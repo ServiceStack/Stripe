e4b9bff4330c0af5f0d638965908d54238cee141
Demis Bellot <demis.bellot@gmail.com>
2014-02-22 09:39:54 -0500

    Upgrade to v4.0.11

--- NuGet/NuGetPush.cmd
+++ NuGet/NuGetPush.cmd
@@ -1,3 +1,3 @@
 SET NUGET=..\src\.nuget\nuget
-%NUGET% push ServiceStack.Stripe.4.0.8.nupkg
-%NUGET% push ServiceStack.Stripe.4.0.8.symbols.nupkg
+%NUGET% push ServiceStack.Stripe.4.0.11.nupkg
+%NUGET% push ServiceStack.Stripe.4.0.11.symbols.nupkg

f91135f0ffb7439c8ccae85536a72c9da77478b2
Demis Bellot <demis.bellot@gmail.com>
2014-04-14 13:37:38 -0400

    Update NuGet to v4.0.18

--- NuGet/NuGetPush.cmd
+++ NuGet/NuGetPush.cmd
@@ -1,6 +1,6 @@
 SET NUGET=..\src\.nuget\nuget
-%NUGET% push ServiceStack.Stripe.4.0.15.nupkg
-%NUGET% push ServiceStack.Stripe.4.0.15.symbols.nupkg
+%NUGET% push ServiceStack.Stripe.4.0.18.nupkg
+%NUGET% push ServiceStack.Stripe.4.0.18.symbols.nupkg
 
-%NUGET% push ServiceStack.Stripe.Pcl.4.0.15.nupkg
-%NUGET% push ServiceStack.Stripe.Pcl.4.0.15.symbols.nupkg
+%NUGET% push ServiceStack.Stripe.Pcl.4.0.18.nupkg
+%NUGET% push ServiceStack.Stripe.Pcl.4.0.18.symbols.nupkg

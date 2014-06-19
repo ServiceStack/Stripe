23396713d5725f7f1a971fccd34911fb4d80d783
Demis Bellot <demis.bellot@gmail.com>
2014-06-18 20:17:11 -0400

    Upgrade to v4.0.22

--- NuGet/NuGetPush.cmd
+++ NuGet/NuGetPush.cmd
@@ -1,7 +1,7 @@
 SET NUGET=..\src\.nuget\nuget
 %NUGET% setApiKey 4793bfcc-06ad-4ec6-bc48-62f514c2aa9a
-%NUGET% push ServiceStack.Stripe.4.0.20.nupkg
-%NUGET% push ServiceStack.Stripe.4.0.20.symbols.nupkg
+%NUGET% push ServiceStack.Stripe.4.0.22.nupkg
+%NUGET% push ServiceStack.Stripe.4.0.22.symbols.nupkg
 
-%NUGET% push ServiceStack.Stripe.Pcl.4.0.20.nupkg
-%NUGET% push ServiceStack.Stripe.Pcl.4.0.20.symbols.nupkg
+%NUGET% push ServiceStack.Stripe.Pcl.4.0.22.nupkg
+%NUGET% push ServiceStack.Stripe.Pcl.4.0.22.symbols.nupkg

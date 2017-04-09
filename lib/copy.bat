3b65c2a787d1e23e95d2e8abcd0a9bbd6b141fb7
Demis Bellot <demis.bellot@gmail.com>
2017-04-09 03:20:03 -0400

    restructure deps into platform folders + remove old proj.json cruft

--- lib/copy.bat
+++ lib/copy.bat
@@ -1,2 +1,15 @@
-COPY C:\src\ServiceStack\src\ServiceStack.Client\bin\Release\ServiceStack.* .
-COPY C:\src\ServiceStack.Text\src\ServiceStack.Text\bin\Release\ServiceStack.Text.* .
+REM SET BUILD=Debug
+SET BUILD=Release
+
+COPY "..\..\ServiceStack\src\ServiceStack.Interfaces\bin\%BUILD%\portable40-net45+sl5+win8+wp8+wpa81\ServiceStack.Interfaces.*" pcl
+
+COPY ..\..\ServiceStack\src\ServiceStack.Client\bin\%BUILD%\net45\ServiceStack.Client.* net45
+COPY ..\..\ServiceStack\src\ServiceStack.Client\bin\%BUILD%\netstandard1.1\ServiceStack.Client.* netstandard1.1
+COPY ..\..\ServiceStack\src\ServiceStack.Client\bin\%BUILD%\netstandard1.6\ServiceStack.Client.* netstandard1.6
+COPY "..\..\ServiceStack\src\ServiceStack.Client\bin\%BUILD%\portable45-net45+win8\ServiceStack.Client.*" pcl
+
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* net45
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\ServiceStack.Text.* netstandard1.1
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\ServiceStack.Text.* netstandard1.3
+COPY "..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\portable45-net45+win8\ServiceStack.Text.*" pcl
+

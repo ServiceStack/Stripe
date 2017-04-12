52ef46ff2e35c4d85d03ff449bb70b5f8bc02691
Demis Bellot <demis.bellot@gmail.com>
2017-04-12 15:34:01 -0400

    Extract PCL into its own csproj

--- lib/copy.bat
+++ lib/copy.bat
@@ -2,6 +2,7 @@ REM SET BUILD=Debug
 SET BUILD=Release
 
 COPY "..\..\ServiceStack\src\ServiceStack.Interfaces\bin\%BUILD%\portable40-net45+sl5+win8+wp8+wpa81\ServiceStack.Interfaces.*" pcl
+COPY ..\..\ServiceStack\src\ServiceStack.Interfaces\bin\%BUILD%\netstandard1.1\ServiceStack.Interfaces.* netstandard1.1
 
 COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* net45
 COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\ServiceStack.Text.* netstandard1.1

f0996e236b46378e10461caa7b08f1f55f3a2e90
Demis Bellot <demis.bellot@gmail.com>
2017-04-12 04:54:48 -0400

    Extract Core projects into separate build-core.proj allow distinct version numbers

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,4 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-%MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=9;PatchCoreVersion=41
+%MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
+%MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9

d7b7f6aa21b51456a7e7ec173c595d3a77a8a3eb
Demis Bellot <demis.bellot@gmail.com>
2017-04-12 04:37:19 -0400

    hard code version in build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-%MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
+%MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=9;PatchCoreVersion=41

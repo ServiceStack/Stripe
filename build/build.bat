1811b8f14685038f7aa70c557df6188f7ec1df5a
Demis Bellot <demis.bellot@gmail.com>
2017-09-04 23:38:29 -0400

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,6 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-%MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
-%MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
+REM %MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
+REM %MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
+
+msbuild /p:Configuration=Release ..\src\Stripe.sln

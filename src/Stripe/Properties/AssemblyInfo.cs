7c3e4884d9c6c801d0db97c42ca185b894578410
Demis Bellot <demis.bellot@gmail.com>
2022-01-24 21:43:41 +0800

    Upgrade to v6

--- src/Stripe/Properties/AssemblyInfo.cs
+++ src/Stripe/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("2d8e5da3-cb60-440b-b629-cb0257fa6264")]
-[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
